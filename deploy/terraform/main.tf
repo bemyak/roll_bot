a23d009c350e421cf52e3516f7e632f291294683
Sergej Gureev <sergej.gureev@relex.fi>
2020-06-06 21:32:22 +0300

    Add ansible configuration

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -50,33 +50,34 @@ resource "oci_core_instance" "monitor" {
 
 }
 
-// https://docs.cloud.oracle.com/en-us/iaas/images/image/548dfe4b-282c-41a0-b151-69a3a4fed8a1/
-// Canonical-Ubuntu-20.04-Minimal-2020.04.23-0
+// https://docs.cloud.oracle.com/en-us/iaas/images/image/7581ea04-f340-418c-a495-2968437bea8e/
+// Canonical-Ubuntu-20.04-Minimal-2020.05.19-0
 locals {
   images = {
-    ap-hyderabad-1   = "ocid1.image.oc1.ap-hyderabad-1.aaaaaaaa5fauvvguntdoat7tishuzvpmtyhczppv7dxo4mmrl6rrdr5mhela"
-    ap-melbourne-1   = "ocid1.image.oc1.ap-melbourne-1.aaaaaaaa4b65f4jd7fmbsnrpvv5yaq7hsmmyv3ekti54updp3w4vmi6szlka"
-    ap-mumbai-1      = "ocid1.image.oc1.ap-mumbai-1.aaaaaaaav4gtyj2rw7vg2laqm5k4ylpafryxa4q5gg46wkxrx7xduz3kwkha"
-    ap-osaka-1       = "ocid1.image.oc1.ap-osaka-1.aaaaaaaabt27yritdv7w4mbo3g5vhv5o3saowepzktvfrhvhl33fak56iwpq"
-    ap-seoul-1       = "ocid1.image.oc1.ap-seoul-1.aaaaaaaafjufd5ugdoe76alz7st6gkiljhcnlhotea2nky4oojjygl62y5hq"
-    ap-sydney-1      = "ocid1.image.oc1.ap-sydney-1.aaaaaaaayizobnw6m3lo34j6j6dtffaluosmgnerq5ravxuhtehdow67ip3q"
-    ap-tokyo-1       = "ocid1.image.oc1.ap-tokyo-1.aaaaaaaat4kfe427svas2tocgp3sz6py6hr3od7tgsohuw43lm3fl3gcioiq"
-    ca-montreal-1    = "ocid1.image.oc1.ca-montreal-1.aaaaaaaaveh5p5e7vzddwt7ekrh7epxbuqbnbf6kkubtnk5hyla5mvkra5ya"
-    ca-toronto-1     = "ocid1.image.oc1.ca-toronto-1.aaaaaaaaaxo4jpb2tojcaai4ex5qf3wdtvmqgcs7ux5dhcmmke3yxnmmozia"
-    eu-amsterdam-1   = "ocid1.image.oc1.eu-amsterdam-1.aaaaaaaawsvb7mxgqkroo6uaqxw3y53jbox3vyksa5znqralfo6o6qur5j6a"
-    eu-frankfurt-1   = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaukhdqqokh3evzyqvashnxld2gyl2wx6k5cratnb7hcxij4u7eh3q"
-    eu-zurich-1      = "ocid1.image.oc1.eu-zurich-1.aaaaaaaawa5o7jwrrrd76i4m4rrox6hmea64d735otloujdimrw7uoj3kr6a"
-    me-jeddah-1      = "ocid1.image.oc1.me-jeddah-1.aaaaaaaaqi45j2kng34kya6o54kfca5vyib72suzk5u4jmyvssy7gljcrcia"
-    sa-saopaulo-1    = "ocid1.image.oc1.sa-saopaulo-1.aaaaaaaat6zascrpkjopqo52cfpyh7pjdt6t6zopqbh5xkhajlyebuediuaa"
-    uk-gov-london-1  = "ocid1.image.oc4.uk-gov-london-1.aaaaaaaambs5vyhf7eck6crcsakzhh4d6jd7hsdvrtbfcza3rve4szs7qifq"
-    uk-london-1      = "ocid1.image.oc1.uk-london-1.aaaaaaaalzhr65u6h3gbq3p2nc2ooke3xss7vkc2riwzsuc66kf75jifthzq"
-    us-ashburn-1     = "ocid1.image.oc1.iad.aaaaaaaaxjheaxicr5omchcu4mxpb3cyivvxb2ptey4x3xtkd5ce5gqbwm2a"
-    us-gov-ashburn-1 = "ocid1.image.oc3.us-gov-ashburn-1.aaaaaaaaxw3uon4fga7zgaznwgwhkai5ppxlixvs67d7vgqzk57fqtvyggha"
-    us-gov-chicago-1 = "ocid1.image.oc3.us-gov-chicago-1.aaaaaaaayi4wxk7xyleqt26fyih3feakvdfxfxintamtji4ws2aghv764ria"
-    us-gov-phoenix-1 = "ocid1.image.oc3.us-gov-phoenix-1.aaaaaaaajrv7qhgeuiaknllm6qdek3bewzp4k5ibhuk5bslrzuhm2nrodjgq"
-    us-langley-1     = "ocid1.image.oc2.us-langley-1.aaaaaaaarvu6zumkvhpjnth6zurajs6f6xbglfkqwc2g7ncjeoonom5gfk7a"
-    us-luke-1        = "ocid1.image.oc2.us-luke-1.aaaaaaaam4as5zbc4rzkgwc62jsgdgjmscmkn7vueihhjqhbwulrxjziswra"
-    us-phoenix-1     = "ocid1.image.oc1.phx.aaaaaaaat3pk73rqpon3ul4prbn7ktul3nedjqadkwf4sjlwqiifyawyjf3a"
+    ap-chuncheon-1   = "ocid1.image.oc1.ap-chuncheon-1.aaaaaaaa2prmyrhymbevpi3cakwlv7plryzn5igclm3yhe6ddwpip2ifsp6a"
+    ap-hyderabad-1   = "ocid1.image.oc1.ap-hyderabad-1.aaaaaaaavlmx2vzskvaok6exy3326emjlvwt2mq4pzedmitay5usaslfewva"
+    ap-melbourne-1   = "ocid1.image.oc1.ap-melbourne-1.aaaaaaaaeoczcg7xj3bk4dwpryhgnq565hazbvhhqwfeflaxoqscdlksucwa"
+    ap-mumbai-1      = "ocid1.image.oc1.ap-mumbai-1.aaaaaaaato3huopeqzvwp235f3nxnoenjlohev3tor7mv7n2vxn5rskcrrdq"
+    ap-osaka-1       = "ocid1.image.oc1.ap-osaka-1.aaaaaaaaygpmwvfp5lvk4dbxuijh7perwtgktbbcsalhoba7dyuc2mbwohwa"
+    ap-seoul-1       = "ocid1.image.oc1.ap-seoul-1.aaaaaaaafrmbd52cxmoyobffrqontmfr6fxshtamouwis2qduazyutmismqq"
+    ap-sydney-1      = "ocid1.image.oc1.ap-sydney-1.aaaaaaaafxql26mepevnjr4em2utv4lxm46ai3ppymfw6bnfrr7bcozjocia"
+    ap-tokyo-1       = "ocid1.image.oc1.ap-tokyo-1.aaaaaaaaqyx3zeas37rdsyaomtz7txwosakqmkrha6ucm646wjlpxazg3wza"
+    ca-montreal-1    = "ocid1.image.oc1.ca-montreal-1.aaaaaaaab2khlxmzrwljhyg7rhgqqgatln2cmk63cpq4judvheda3xupqplq"
+    ca-toronto-1     = "ocid1.image.oc1.ca-toronto-1.aaaaaaaabs2t6xawpabyjw3lx62hyogvu7say2u2ve65yzt3fw7idpingocq"
+    eu-amsterdam-1   = "ocid1.image.oc1.eu-amsterdam-1.aaaaaaaawlskfep4prrqzp5ooj2j4lqprfefjqzgsulcdulrxhzqmaqqddbq"
+    eu-frankfurt-1   = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaawnnvmhojtfjvmsxuklektxhpmfhbofn4zunbv7waqmmf7z3oteba"
+    eu-zurich-1      = "ocid1.image.oc1.eu-zurich-1.aaaaaaaa5a35uvzfrbqxykqsf3vgdnnrbduj7k7t5hgas26hp2seq77cbsrq"
+    me-jeddah-1      = "ocid1.image.oc1.me-jeddah-1.aaaaaaaap5qszhdu5zh6fjd64734zevnxaap6seqlbe7fnxom3ppgpl5sdmq"
+    sa-saopaulo-1    = "ocid1.image.oc1.sa-saopaulo-1.aaaaaaaack6sclj2pjw2c5nsr2fw3orh3yofegfgo2b5dikamllfykrqqzta"
+    uk-gov-london-1  = "ocid1.image.oc4.uk-gov-london-1.aaaaaaaadoahmg7nlyuf3ragxinmrrdyo4gyavxhy3jutpwnc4p45av3lycq"
+    uk-london-1      = "ocid1.image.oc1.uk-london-1.aaaaaaaadwwv7j2gjjhyhtren6gtwzn24rxf42cl75x42ycvwu4rehtjbw3q"
+    us-ashburn-1     = "ocid1.image.oc1.iad.aaaaaaaad5yt6qrkhk3hur3ld26zjeyljhd4ghvevztli466sedo42tthjua"
+    us-gov-ashburn-1 = "ocid1.image.oc3.us-gov-ashburn-1.aaaaaaaaxdft75dlj7jvkekmqkoz3xxinmk55ygkmkupqkqlpkbwlsh3dtzq"
+    us-gov-chicago-1 = "ocid1.image.oc3.us-gov-chicago-1.aaaaaaaaaymekedikn6tzbelejgmfcc6dv2w6xv3kfi65p4xc73m4fkfae4a"
+    us-gov-phoenix-1 = "ocid1.image.oc3.us-gov-phoenix-1.aaaaaaaahfg6snrlxbwj5g4r2ou54syr7xvwvs3dclbx4qjjls6szntodtaa"
+    us-langley-1     = "ocid1.image.oc2.us-langley-1.aaaaaaaa4omqkjj3ia4jqjzlcyzrqarwo3fum5crkzo43i673ufgxqcivlmq"
+    us-luke-1        = "ocid1.image.oc2.us-luke-1.aaaaaaaax76u2ivdza273clzzec3aqgiljjw5fu6pp2jtd6bwdziaseayn6a"
+    us-phoenix-1     = "ocid1.image.oc1.phx.aaaaaaaabrsxasb7lpykfx5hzfss6n644ffx25m5ymy3najhoyebemfofsba"
   }
 
   instance_shape = "VM.Standard.E2.1.Micro"
--- deploy/terraform/outputs.tf
+++ deploy/terraform/outputs.tf
@@ -5,3 +5,11 @@ output "monitor_public_ip" {
 output "rollbot_public_ip" {
   value = oci_core_instance.rollbot.public_ip
 }
+
+output "monitor_private_ip" {
+  value = oci_core_instance.monitor.private_ip
+}
+
+output "rollbot_private_ip" {
+  value = oci_core_instance.rollbot.private_ip
+}
